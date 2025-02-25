de24b74e52e713fc491a1727147f109fa2e7ab36
mbessarab <maks@besssarab.ru>
2025-02-25 09:56:45 +0300

    refactoring filter

--- src/telegram/middleware/filter.mjs
+++ src/telegram/middleware/filter.mjs
@@ -1,4 +1,10 @@
-import {checkAvailableUsers, isUser} from "./userHelper.mjs"
+export const isUser = (ctx) => !ctx.from.is_bot
+
+export const isOtherBot = (ctx) => !isUser && (ctx.from.id !== ctx.me.id)
+
+export const isMeAsBot = (ctx) => !isOtherBot(ctx)
+
+export const checkAvailableUsers = (ctx) =>  isUser(ctx) || isMeAsBot(ctx)
 
 // Отфильтровать сообщения от других ботов
 export const filterBotsMiddleware =  async (ctx) => checkAvailableUsers(ctx)
--- src/telegram/middleware/userHelper.mjs
+++
@@ -1,7 +0,0 @@
-export const isUser = (ctx) => !ctx.from.is_bot
-
-export const isOtherBot = (ctx) => !isUser && (ctx.from.id !== ctx.me.id)
-
-export const isMeAsBot = (ctx) => !isOtherBot(ctx)
-
-export const checkAvailableUsers = (ctx) =>  isUser(ctx) || isMeAsBot(ctx)
